c415fdd5786d42f7d6124278779f940388f28aab
David Vaykhanskiy <dvaykhanskiy@ucvts.tec.nj.us>
2019-11-01 08:57:23 -0400

    Exercises 2 - 4 work.

--- src/ProblemSet5.java
+++ src/ProblemSet5.java
@@ -20,6 +20,7 @@ public class ProblemSet5 {
 
     public static void main(String[] args) {
         ProblemSet5 ps = new ProblemSet5();
+        System.out.println(ps.isCentered("1234567", "355"));
     }
 
     /*
@@ -30,17 +31,16 @@ public class ProblemSet5 {
      */
 
     public String surroundMe(String in, String out) {
-      if (if == null) {
+      if (in == null) {
         return null;
       } else if (in != null && out != null && out.length() == 4) {
-        String out1 = out.substring(0, 1);
-        String out2 = out.substring(2, 3);
-        System.out.println(out1 + in +  out2);
+        String out1 = out.substring(0, 2);
+        String out2 = out.substring(2, 4);
+        return out1 + in +  out2;
       } else {
-        System.out.println(in);
+        return in;
       }
     }
-
     /*
      * Exercise 2.
      *
@@ -51,7 +51,11 @@ public class ProblemSet5 {
     public String endsMeet(String text, int n) {
       if (text == null) {
         return null;
-      } else if ()
+      } else if ((text.length() > 0 && text.length() < 10) && (n > 0 && n < text.length())) {
+        return text.substring(0, n) + text.substring(text.length() - n, text.length());
+      } else {
+        return text;
+      }
     }
 
     /*
@@ -61,7 +65,12 @@ public class ProblemSet5 {
      */
 
     public String middleMan(String text) {
-
+      if (text.length() % 2 == 1) {
+        int middleChar = text.length() / 2;
+        return text.substring(middleChar - 1, middleChar + 2);
+      } else {
+        return text;
+      }
     }
 
     /*
@@ -72,67 +81,77 @@ public class ProblemSet5 {
      */
 
     public boolean isCentered(String text, String target) {
-
+      if (text.length() % 2 == 1 && target.length() == 3) {
+        int middleChar = text.length() / 2;
+        String middleChars = text.substring(middleChar - 1, middleChar + 2);
+        if (target.equals(middleChars)) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
     }
 
     /*
-     * Exercise 5.
-     *
-     * Given a string and a character, compute the number of words that end in suffix.
-     */
-
-    public int countMe(String text, char suffix) {
-
-    }
-
-    /*
-     * Exercise 6.
-     *
-     * Given a string, compute the number of triplets in text.
-     */
-
-    public int triplets(String text) {
-
-    }
-
-    /*
-     * Exercise 7.
-     *
-     * Given a string, compute the sum of the digits in text.
-     */
-
-    public long addMe(String text) {
-
-    }
-
-    /*
-     * Exercise 8.
-     *
-     * Given a string, compute the length of the longest sequence.
-     */
-
-    public long sequence(String text) {
-
-    }
-
-    /*
-     * Exercise 9.
-     *
-     * Given two strings, return a new string built by intertwining each of the
-     * characters of a and b.
-     */
-
-    public String intertwine(String a, String b) {
-
-    }
-
-    /*
-     * Exercise 10.
-     *
-     * Given a string, determine whether or not it is a palindrome.
-     */
-
-    public boolean isPalindrome(String text) {
-
-    }
+    //  * Exercise 5.
+    //  *
+    //  * Given a string and a character, compute the number of words that end in suffix.
+    //  */
+    //
+    // public int countMe(String text, char suffix) {
+    //
+    // }
+    //
+    // /*
+    //  * Exercise 6.
+    //  *
+    //  * Given a string, compute the number of triplets in text.
+    //  */
+    //
+    // public int triplets(String text) {
+    //
+    // }
+    //
+    // /*
+    //  * Exercise 7.
+    //  *
+    //  * Given a string, compute the sum of the digits in text.
+    //  */
+    //
+    // public long addMe(String text) {
+    //
+    // }
+    //
+    // /*
+    //  * Exercise 8.
+    //  *
+    //  * Given a string, compute the length of the longest sequence.
+    //  */
+    //
+    // public long sequence(String text) {
+    //
+    // }
+    //
+    // /*
+    //  * Exercise 9.
+    //  *
+    //  * Given two strings, return a new string built by intertwining each of the
+    //  * characters of a and b.
+    //  */
+    //
+    // public String intertwine(String a, String b) {
+    //
+    // }
+    //
+    // /*
+    //  * Exercise 10.
+    //  *
+    //  * Given a string, determine whether or not it is a palindrome.
+    //  */
+    //
+    // public boolean isPalindrome(String text) {
+    //
+    // }
 }
